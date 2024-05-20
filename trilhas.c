39f94c127d8c6273ccb0ce6bb9e86dc3af581a61
Ítalo Gabriel <159637560+italocas@users.noreply.github.com>
2024-05-20 14:47:24 -0300

    Add files via upload

---
+++ trilhas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+
+float n1 , n2, media;
+
+    printf("Insira a primeira nota do aluno:\n");
+        scanf( "%f" , &n1);
+    printf("Insira a segunda nota do aluno:\n");
+        scanf( "%f" , &n2);
+media = (n1 + n2) / 2 ;
+
+if (media > 7) {
+    printf ("O aluno foi reprovado com a media de %.f:\n" , media );
+}
+else{ printf("O aluno foi aprovado com a media de %.f:\n" , media) ;
+}
+return  0;
+}
